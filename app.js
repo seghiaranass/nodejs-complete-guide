bfde22d45c664a99328d6e9de2a4c36984595733
seghiaranass <anass.segh97@gmail.com>
2025-02-04 00:20:56 +0100

    Using Express Router

--- app.js
+++ app.js
@@ -1,26 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
-
 app.use(bodyParser.urlencoded({extended: false}));
 
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
-app.use('/add-product',(req,res,next)=>{
-    console.log('In the another middleware!');
-    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
-});
-
-
-app.post('/product',(req,res,next)=>{    
-    console.log(req.body);
-    res.redirect('/add-product');
-  });
-
-
-app.use('/',(req,res,next)=>{
-    console.log('In the another middleware!');
-    res.send('<h1>Hello from Express!</h1>');
-});
+app.use(adminRoutes);
+app.use(shopRoutes);
 
 
 
---
+++ routes/shop.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const router = express.Router();
+
+router.get('/',(req,res,next)=>{
+    console.log('In the another middleware!');
+    res.send('<h1>Hello from Express! </h1> <a href="/add-product">Add Product</a>');
+});
+
+
+module.exports = router;
