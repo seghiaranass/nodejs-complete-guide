9b543e200caf7cf9840827b90c2c62dddb04a60a
seghiaranass <anass.segh97@gmail.com>
2025-02-03 23:52:50 +0100

    Use app.listen() without createHttpServer function

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-const http = require('http');
-
 const express = require('express');
 
 const app = express();
@@ -12,11 +10,11 @@ app.use((req,res,next)=>{
 
 app.use((req,res,next)=>{
     console.log('In the another middleware!');
+    res.send('<h1>Hello from Express!</h1>');
 });
 
 
-const server = http.createServer(app);
 
-server.listen(3001);
+app.listen(3001);
 
 
