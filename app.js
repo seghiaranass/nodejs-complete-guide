a7e3bb85610cacf129638cc39c51d187caf23f50
seghiaranass <anass.segh97@gmail.com>
2025-02-03 23:46:45 +0100

    Using Middlewares

--- app.js
+++ app.js
@@ -4,8 +4,19 @@ const express = require('express');
 
 const app = express();
 
+app.use((req,res,next)=>{
+
+    console.log('In the middleware!');
+    next();
+});
+
+app.use((req,res,next)=>{
+    console.log('In the another middleware!');
+});
+
+
 const server = http.createServer(app);
 
-server.listen(3000);
+server.listen(3001);
 
 
